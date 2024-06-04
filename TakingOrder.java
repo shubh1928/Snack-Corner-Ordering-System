74b313c77e2a36f5e20e0121266fb2ca8105829c
Shubham Gopale <shubhamgopale16@gmail.com>
2024-06-04 21:22:48 +0530

    Order Taking Code Added

---
+++ TakingOrder.java
@@ -0,0 +1,12 @@
+package Snack-Corner-Ordering-System;
+
+public class TakingOrder {
+    public static void main(String[] args) {
+
+        FoodOrder f = new FoodOrder();
+
+        f.displayMenu();
+        f.order();
+
+    }
+}
